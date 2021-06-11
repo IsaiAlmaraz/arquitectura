fe7f50e8b8f89ff35faf59f2ef59f53799c2738a
Isai Almaraz <34081603+IsaiAlmaraz@users.noreply.github.com>
2021-06-11 13:00:39 -0500

    route ArtistaController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GenereController;
+use App\Http\Controllers\ArtistaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
